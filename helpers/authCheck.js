bd660c58ebb0b950075d0d396c2ec5a4ff62026e
Dima <dimm1simm1@gmail.com>
2020-01-30 14:34:17 +0200

    cors

--- helpers/authCheck.js
+++ helpers/authCheck.js
@@ -2,6 +2,7 @@
 (чи є його дані в кукіз сесії)
 */
 exports.checkIfAuthenticated = (req, res, next) =>{
+  console.log(111111111,req.headers.authorization);
   if(req.headers.authorization) return next();
   if (req.isAuthenticated()) { return next(); }
   res.status(401).send('Not authorised');
