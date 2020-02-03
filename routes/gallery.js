bdd09770aa89e9fbb9f3ad3b25f72fe9db3c7ef1
Dima <dimm1simm1@gmail.com>
2020-02-03 17:30:38 +0200

    cloud

--- routes/gallery.js
+++ routes/gallery.js
@@ -7,18 +7,18 @@ const {addGallery,
        deleteGallery} = require('../controllers/gallery');
 
 const {catchErrors} = require('../errors/errorHandler');
-const { upload, cloud} = require('../helpers/multer');
+const {uploadNone, upload, cloud} = require('../helpers/multer');
 const {checkIfAuthenticated} = require('../helpers/authCheck');
 const router = express.Router();
 
 router.route('/gallery')
     .get(catchErrors(getGalleryList));
 router.route('/gallery/update')
-.post(  upload, cloud,  catchErrors(editGallery) )
+.post(  upload,  cloud,  catchErrors(editGallery) )
 router.route('/gallery/single')
     .get( [check('id').isMongoId()],
         catchErrors(getSingleGallery) )
-    .post(  upload, cloud,    catchErrors(addGallery) )
+    .post(  upload,  cloud,   catchErrors(addGallery) )
     .delete( [check('id').isMongoId()],
         catchErrors(deleteGallery));
 
